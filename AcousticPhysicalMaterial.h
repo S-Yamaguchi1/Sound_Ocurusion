7eae814bdefa3a947d84a7b39016a94eb9767474
S-Yamaguchi1 <sakanasakana109@gmail.com>
2026-07-14 14:01:38 +0900

    Update AcousticPhysicalMaterial.h
    コメント修正

--- AcousticPhysicalMaterial.h
+++ AcousticPhysicalMaterial.h
@@ -1,11 +1,14 @@
 // 音響特性を持たせた Physical Material。
 // 壁に割り当てることで、その面の透過特性を遮蔽計算に反映できる。
+
+//一度だけ読み込み
 #pragma once
 
 #include "CoreMinimal.h"
 #include "PhysicalMaterials/PhysicalMaterial.h"
 #include "AcousticPhysicalMaterial.generated.h"
 
+//UEの機能を使用する
 UCLASS()
 class SOUNDOCCLUSION_API UAcousticPhysicalMaterial : public UPhysicalMaterial
 {
@@ -13,7 +16,7 @@ class SOUNDOCCLUSION_API UAcousticPhysicalMaterial : public UPhysicalMaterial
 
 public:
     // 幾何的な厚みに掛ける重み。大きいほど強く遮蔽する。
-    // 目安: カーテン 0.3 / 木材 1.0 / コンクリート 3.0 / 金属 4.0
+    // カーテン 0.3 / 木材 1.0 / コンクリート 3.0 / 金属 4.0
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Acoustics",
               meta = (ClampMin = "0.0"))
     float AcousticDensity = 1.0f;
